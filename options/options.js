13e98f177a941eb7f954afcfb71579bb326acb6c
Siddhartha Sarkar <siddsarkar7@gmail.com>
2020-09-07 14:20:27 +0530

    comments for code readability

--- options/options.js
+++ options/options.js
@@ -1,4 +1,4 @@
-//get dom elements
+//get elements of options page
 const usernameInput = document.querySelector("#username");
 const passwordInput = document.querySelector("#password");
 const _q1 = document.querySelector("#q1");
@@ -7,36 +7,32 @@ const _q2 = document.querySelector("#q2");
 const _a2 = document.querySelector("#a2");
 const _q3 = document.querySelector("#q3");
 const _a3 = document.querySelector("#a3");
-const head = document.getElementById("head");
 const body = document.querySelector("body");
-let reset = document.getElementById("reset");
 
+// Open ERP button
 const btnerp = document.getElementById("erpBtn");
-
+function callback() {
+  window.close();
+}
 function openThis(callback) {
   window.open("https://erp.iitkgp.ac.in/");
   window.focus();
   callback();
 }
-
-function callback() {
-  window.close();
-}
 btnerp.addEventListener("click", () => openThis(callback));
 
-// secret theme implementation
-
-head.addEventListener("click", (e) => {
-  e.preventDefault();
-  browser.storage.local.get().then((res) => {
-    toggler(res.authCredentials);
-  });
-});
-
+// Theme toggle Button
+const head = document.getElementById("head");
+function theme(isdark) {
+  if (isdark) {
+    body.setAttribute("style", "background:#1B1B1B; color: whitesmoke");
+  } else {
+    body.removeAttribute("style");
+  }
+}
 function toggler(isdark) {
   const data = isdark;
-  //check current theme
-  // console.log(!isdark);
+  //check current theme(!isdark)
   browser.storage.local.set({
     authCredentials: {
       ...data,
@@ -45,17 +41,16 @@ function toggler(isdark) {
   });
   theme(!isdark.dark);
 }
+head.addEventListener("click", (e) => {
+  e.preventDefault();
+  browser.storage.local.get().then((res) => {
+    toggler(res.authCredentials);
+  });
+});
 
-function theme(isdark) {
-  if (isdark) {
-    body.setAttribute("style", "background:#1B1B1B;color: whitesmoke");
-  } else {
-    body.removeAttribute("style");
-  }
-}
-
-// reset button
-reset.addEventListener("click", (cleanup) => {
+// Reset button
+const reset = document.getElementById("reset");
+reset.addEventListener("click", () => {
   browser.storage.local.set({
     authCredentials: {
       username: "",
@@ -74,7 +69,7 @@ reset.addEventListener("click", (cleanup) => {
   });
   creating.then(onCreated, onError);
   function onCreated(tab) {
-    // console.log(`Created new tab: ${tab.id}`);
+    console.log(`Created new tab: ${tab.id}`);
   }
 });
 
@@ -89,13 +84,12 @@ function getQuestions() {
     http.open("POST", url, false);
     http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     http.onreadystatechange = function () {
-      //Call a function when the state changes.
       if (
         http.readyState == 4 &&
         http.status == 200 &&
         http.responseText != "FALSE"
       ) {
-        // console.log(http.responseText);
+        // console.log here to see respose
         if (_q1.value == "") {
           _q1.value = http.responseText;
         } else if (_q2.value == "" && http.responseText != _q1.value) {
@@ -113,10 +107,7 @@ function getQuestions() {
   }
 }
 
-/*
-Store the currently selected settings using browser.storage.local.
-*/
-
+// Store the currently typed information on blur event
 function storeSettings() {
   browser.storage.local.set({
     authCredentials: {
@@ -132,10 +123,13 @@ function storeSettings() {
   });
 }
 
-/*
-Update the options UI with the settings values retrieved from storage,
-or the default settings if the stored settings are empty.
-*/
+// error-logger
+function onError(e) {
+  console.error(e);
+}
+
+//Update the options UI with the settings values retrieved from storage,
+//or the default settings if the stored settings are empty.
 function updateUI(restoredSettings) {
   theme(restoredSettings.authCredentials.dark);
   usernameInput.value = restoredSettings.authCredentials.username || "";
@@ -147,23 +141,14 @@ function updateUI(restoredSettings) {
   _q3.value = restoredSettings.authCredentials.q3 || "";
   _a3.value = restoredSettings.authCredentials.a3 || "";
 }
-function onError(e) {
-  console.error(e);
-}
-
-/*
-On opening the options page, fetch stored settings and update the UI with them.
-*/
+// On opening the options page, fetch stored settings and update the UI with them.
 const gettingStoredSettings = browser.storage.local.get();
 gettingStoredSettings.then(updateUI, onError);
 
-/*
-To get questions
-*/
+// To get questions
 usernameInput.addEventListener("blur", getQuestions);
-/*
-On blur, save the currently selected settings.
-*/
+
+//On blur, save the currently selected settings.
 usernameInput.addEventListener("blur", storeSettings);
 passwordInput.addEventListener("blur", storeSettings);
 _q1.addEventListener("blur", storeSettings);
--- storage.js
+++ storage.js
@@ -1,3 +1,4 @@
+// grab elements of the page
 const loginid = document.getElementById("user_id");
 const quesn = document.getElementById("question");
 const answer = document.getElementById("answer");
@@ -5,25 +6,31 @@ const pass = document.getElementById("password");
 const inject = document.getElementById("signin");
 const btn = document.getElementsByClassName("btn btn-primary");
 
+//process of filling info to the page
 function process() {
   browser.storage.local.get().then((res) => {
     authstart(res.authCredentials);
   });
 }
 
+// case-1 refresh on page
 if (document.readyState == "complete") {
   process();
 }
 
-function stateEvent(e) {
+//case-2 on new tab creation
+function stateEvent() {
   document.removeEventListener("readystatechange", stateEvent);
   if (document.readyState == "complete") {
     process();
   }
 }
+
+//listen to state of page
 document.addEventListener("readystatechange", stateEvent);
 
 function authstart(cred) {
+  //check if all fields are filled.
   if (
     cred.username != "" &&
     cred.password != "" &&
@@ -58,6 +65,7 @@ function authstart(cred) {
       }
     }, 1000);
   } else {
+    //if not show error msg on the page.
     const msg = document.createElement("div");
     msg.setAttribute(
       "style",
@@ -71,9 +79,7 @@ function authstart(cred) {
   }
 }
 
-/*
-Default settings. Initialize storage to these values.
-*/
+//Default settings. Initialize storage to these values.
 let authCredentials = {
   username: "",
   password: "",
@@ -86,20 +92,15 @@ let authCredentials = {
   dark: false,
 };
 
-/*
-Generic error logger.
-*/
+//error-logger
 function onError(e) {
   console.error(e);
 }
 
-/*
-On startup, check whether we have stored settings.
-If we don't, then store the default settings.
-*/
-
+//On startup, check whether we have stored settings.
 function checkStoredSettings(storedSettings) {
   if (!storedSettings.authCredentials) {
+    //If we don't, then store the default settings.
     browser.storage.local.set({ authCredentials });
   }
 }
