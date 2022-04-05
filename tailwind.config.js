fda6516f01bd411efed28e4515fc05a73f66aced
Siddhartha Sarkar <siddsarkar7@gmail.com>
2022-04-05 21:57:53 +0530

    add:
    - themed backgrounds
    - ui rework

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
     content: ["./src/pages/**/*.{html,js}"],
     theme: {
+        colors: {
+            primary: "#5c6ac4",
+            secondary: "#ecc94b",
+            ...colors,
+        },
         extend: {},
     },
     darkMode: "class",
